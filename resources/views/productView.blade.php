d81f31aec75cede98f9d44bac15c3666843645fb
Vashu Yadav <vashuy5@gmail.com>
2021-08-20 19:20:16 +0530

    make mail template

--- resources/views/productView.blade.php
+++ resources/views/productView.blade.php
@@ -4,6 +4,46 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
+
+                <div style="border-top: solid 6px black; margin: 0px 40px;">
+                    <div style="border: solid 1px black;">
+                        <table border="0" width="100%">
+                            <tr>
+                                <td align="center">
+                                    <h2 style="font-size: 60px;"><strong>D</strong></h2>
+                                    <h2>Welcome to Product</h2><br><br><br>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Hello name
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="padding: 30px 5px;">
+                                    Thank you for signing up for product. We`re really happy to have you!
+                                    <br>Click the link below to login to your account:
+                                </td>
+                            </tr>
+                            <tr>
+                                <td align="center" style="padding: 30px 5px;">
+                                    <a href="#" style="padding: 10px; background-color: forestgreen; color: white; border-radius: 5px;">Login to Your Account</a>
+                                </td>
+                            </tr>
+
+                            <tr><td style="padding: 30px 5px;">Best regards,</td></tr>
+                            <tr>
+                                <td>
+                                    Casaba Kissi<br>
+                                    Elerion ltd., CEO and fouder
+                                    <br><br><br>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+
+                <br>
                 <div class="panel panel-default">
                     <div class="panel-heading">Product List</div>
 
@@ -14,7 +54,7 @@
                             </div>
                         @endif
 
-                            <table width="100%" border="1">
+                            {{--<table width="100%" border="1">
                                 <tr style="background-color: lightblue;">
                                     <td><strong>User Name</strong></td>
                                     <td><strong>Email</strong></td>
@@ -37,7 +77,7 @@
                                     </tr>
                                 @endforeach
 
-                            </table>
+                            </table>--}}
                     </div>
                 </div>
             </div>
